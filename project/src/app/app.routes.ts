f90cbdc260bd6124831bac27b2c761217a3bc9a4
JadeLuke <jadeluke.booysen@gmail.com>
2025-07-30 15:29:37 +0200

    added route to sponsor request form

--- project/src/app/app.routes.ts
+++ project/src/app/app.routes.ts
@@ -5,6 +5,10 @@ import { Register } from './component/register/register';
 import { VerifyEmail } from './component/verify-email/verify-email';
 import { SponsorDasboardComponent } from './component/sponsor-dasboard/sponsor-dasboard.component';
 import { IndividualDasboardComponent } from './component/individual-dasboard/individual-dasboard.component';
+import { ForgotPassword } from './component/forgot-password/forgot-password';
+import { OrganisationDashboardComponent } from './component/organisation-dashboard/organisation-dashboard.component';
+import { ResetPassword } from './component/reset-password/reset-password';
+import { SponsorRequest } from './Pages/sponsor-request/sponsor-request';
 
 
 
@@ -15,12 +19,13 @@ export const routes: Routes = [
   { path: 'sign-in', component: SignInComponent },
   { path: 'register', component: Register },
   { path: 'verify-email', component: VerifyEmail },
-  { path: 'Landing', component: LandingComponent },
+  { path: 'landing', component: LandingComponent },
 
   
   { path: 'forgot-password', component: ForgotPassword },
   { path: 'auth/reset-password', component: ResetPassword },
   {path: 'sponsor-dashboard',component: SponsorDasboardComponent},
   {path: 'individual-dashboard',component: IndividualDasboardComponent},
-  {path: 'organisation-dashboard', component: OrganisationDashboardComponent}
+  {path: 'organisation-dashboard', component: OrganisationDashboardComponent},
+  {  path: 'req', component: SponsorRequest}
 ];
