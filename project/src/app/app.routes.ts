f16937f4480899f4599806d5546531aaf1ca5a81
Mbali-Nyalungu <Mbali.Nyalungu04@gmail.com>
2025-08-04 09:51:47 +0200

    fixed name to userna

--- project/src/app/app.routes.ts
+++ project/src/app/app.routes.ts
@@ -5,6 +5,10 @@ import { Register } from './component/register/register';
 import { VerifyEmail } from './component/verify-email/verify-email';
 import { SponsorDasboardComponent } from './component/sponsor-dasboard/sponsor-dasboard.component';
 import { IndividualDasboardComponent } from './component/individual-dasboard/individual-dasboard.component';
+import { ResetPassword } from './component/reset-password/reset-password';
+import { OrganisationDashboardComponent } from './component/organisation-dashboard/organisation-dashboard.component';
+import { ForgotPassword } from './component/forgot-password/forgot-password';
+import { SponsorRequest } from './Pages/sponsor-request/sponsor-request';
 
 
 
@@ -15,7 +19,7 @@ export const routes: Routes = [
   { path: 'sign-in', component: SignInComponent },
   { path: 'register', component: Register },
   { path: 'verify-email', component: VerifyEmail },
-  { path: 'Landing', component: LandingComponent },
+  { path: '', component: LandingComponent },
 
   
   { path: 'forgot-password', component: ForgotPassword },
