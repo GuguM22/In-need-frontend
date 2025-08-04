f6426a7167a3b5f110ff09e32c0df8798c7edc11
Sibonelo Mthunzi <hope.mthunzim5@gmail.com>
2025-08-04 14:53:56 +0200

    creating a navbar, footer and sidebar

--- project/src/app/app.ts
+++ project/src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Navbar } from './ui/navbar/navbar'; 
+import { Navbar } from './ui/navbar/navbar';
+import { FooterComponent } from "./ui/footer/footer"; 
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,Navbar],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
