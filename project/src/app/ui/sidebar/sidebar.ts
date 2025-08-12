ec1d854d8c8df8ff2ca55c226bb9741444b19c77
Ganeth <83027242+Seetsi1997@users.noreply.github.com>
2025-08-12 14:20:23 +0200

    Added Logout Page

--- project/src/app/ui/sidebar/sidebar.ts
+++ project/src/app/ui/sidebar/sidebar.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { Navbar } from "../navbar/navbar";
 import { CommonModule } from '@angular/common';
 import { Logout } from "../../component/logout/logout";
+import { Services } from '../../service/services';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -13,9 +15,35 @@ import { Logout } from "../../component/logout/logout";
 
 })
 export class Sidebar {
-  toggle = true; 
+  toggle = true;
+  showLogoutModal = false;
+
+  constructor(private userService: Services, private router: Router) {}
 
   handleToggle() {
-    this.toggle = !this.toggle; 
+    this.toggle = !this.toggle;
+  }
+
+  openLogoutModal() {
+     this.toggle = false; 
+    this.showLogoutModal = true;
+  }
+
+  closeLogoutModal() {
+    this.showLogoutModal = false;
+    this.router.navigate(['/sponsor-dashboard']);
+  }
+
+  confirmLogout() {
+    this.userService.logout().subscribe({
+      next: () => {
+        this.showLogoutModal = false;
+        this.router.navigate(['/sign-in']);
+      },
+      error: (err) => {
+        console.error('Logout failed:', err);
+        this.router.navigate(['/sign-in']);
+      },
+    });
   }
 }
