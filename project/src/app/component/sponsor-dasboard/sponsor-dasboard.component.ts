dede54dfcf2a36bf7863053f5d4606799e29f576
JadeLuke <jadeluke.booysen@gmail.com>
2025-08-13 11:25:17 +0200

    changed styling on dashboards

--- project/src/app/component/sponsor-dasboard/sponsor-dasboard.component.ts
+++ project/src/app/component/sponsor-dasboard/sponsor-dasboard.component.ts
@@ -1,10 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Navbar } from "../../ui/navbar/navbar";
+import { FooterComponent } from '../../ui/footer/footer';
 
 @Component({
   selector: 'app-sponsor-dasboard',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, Navbar, FooterComponent],
   templateUrl: './sponsor-dasboard.component.html',
   styleUrls: ['./sponsor-dasboard.component.css']
 })
