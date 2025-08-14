fa0045b8ffe6f7259a7c65c888bc3c896a66eca0
JadeLuke <jadeluke.booysen@gmail.com>
2025-08-14 10:55:16 +0200

    added routerlink on sponsor dashboard to donate

--- project/src/app/component/sponsor-dasboard/sponsor-dasboard.component.ts
+++ project/src/app/component/sponsor-dasboard/sponsor-dasboard.component.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Navbar } from "../../ui/navbar/navbar";
 import { FooterComponent } from '../../ui/footer/footer';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-sponsor-dasboard',
   standalone: true,
-  imports: [CommonModule, Navbar, FooterComponent],
+  imports: [CommonModule, Navbar, FooterComponent, RouterLink],
   templateUrl: './sponsor-dasboard.component.html',
   styleUrls: ['./sponsor-dasboard.component.css']
 })
